71fbab89c50a7fb11d280aa6bdbcb42b2aab7896
Alysia Dugan <81889411+alysiadugan4@users.noreply.github.com>
2021-10-23 22:36:08 -0700

    Initial bar chart code

--- static/js/app.js
+++ static/js/app.js
@@ -35,13 +35,74 @@ function loadCharts(id) {
     // Meaning you need to for each of the 3 arrays you need to do .slice(0,10).reverse()
     // For the bar chart, x is going to be the sample_values, y is going to be the otu_ids, and text is going to be
     // otu_labels
-    
-    // Now I have everything to do plotly
-    // set up the trace, layout, Plotly.newPlot("bar",yourDataName, yourLayoutName)
+
+    let dropdown = d3.select("#selDataset");
+    //for drop down
+    data.names.forEach((id) => {
+        // console.log(id);
+        
+        dropdown.append("option").text(id).property("value", id);
+    });
+    TopSamples(data.names[0]);
+    DemographicInfo(data.names[0]);
+    BubbleChart(data.names[0]);
+});
+
+function optionChanged(selectedID){
+    // console.log(selectedID);
+
+    TopSamples(selectedID);
+    DemographicInfo(selectedID);
+    BubbleChart(selectedID);
+    // LoadCharts(selectedID);
+}
+
+function TopSamples(id)
+{
+  d3.json("samples.json").then(data => {
+      // console.log(data);
+      let filteredData =data.samples.filter(obj => obj.id == id);
+
+      // console.log(filteredData);
+
+      let otu_ids = filteredData[0].otu_ids.map(otu=> "OTU " + otu);
+      let otu_labels = filteredData[0].otu_labels;
+      let sample_values = filteredData[0].sample_values;
+      // console.log(otu_ids);
+      // console.log(otu_labels);
+      // console.log(sample_values);
+
+      let otu_ids_slice = otu_ids.slice(0,10).reverse();
+      let otu_labels_slice = otu_labels.slice(0,10).reverse();
+      let sample_values_slice = sample_values.slice(0,10).reverse();
+
+      let traceBar = 
+      {
+        x: sample_values_slice,
+        y: otu_ids_slice,
+        text: otu_labels_slice,
+        type: "bar",
+        orientation: "h"
+      };
+
+      // Data trace array
+      let traceData = [traceBar];
+
+      // Apply the group barmode to the layout
+      let layout = 
+      {
+        title: "Top 10 Samples"
+      };
+
+      // Render the plot to the div tag with id "plot"
+      Plotly.newPlot("bar", traceData, layout);
+    }
+  );
+}
 
     //Now do the bubble chart, but it uses all of the data, also don't need to do the .map
 
-
+    
     //Now do the metadata demo panel at sample-metadata
     let filteredMetaData = data.metadata.filter(obj => obj.id == id);
 
@@ -53,13 +114,6 @@ function loadCharts(id) {
     // panel.append("h5").text(key + ": " + value)
 
 
-
-
-
-
-
-  })
-}
 
 d3.json("samples.json").then((data) => {
 
@@ -76,7 +130,6 @@ d3.json("samples.json").then((data) => {
 })
 
 
-function optionChanged(selectedID) {
+function optionChanged(selectedID) 
 
-  loadCharts(selectedID)
-}
+  loadCharts(selectedID)}
