7ce00f463ca84eb53f133f59864de41e7978339a
Alysia Dugan <81889411+alysiadugan4@users.noreply.github.com>
2021-10-24 10:45:48 -0700

    Chart update

--- static/js/app.js
+++ static/js/app.js
@@ -36,72 +36,40 @@ function loadCharts(id) {
     // For the bar chart, x is going to be the sample_values, y is going to be the otu_ids, and text is going to be
     // otu_labels
 
-    let dropdown = d3.select("#selDataset");
-    //for drop down
-    data.names.forEach((id) => {
-        // console.log(id);
-        
-        dropdown.append("option").text(id).property("value", id);
-    });
-    TopSamples(data.names[0]);
-    DemographicInfo(data.names[0]);
-    BubbleChart(data.names[0]);
-});
+    let bar = [{
+      type: 'bar',
+      x: sample_values.slice(0,10).reverse(),
+      y: otu_ids.map(otu => "OTU " + otu).slice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
+      orientation: 'h',
+      order: 'ascending'
+    }];
+    
+    Plotly.newPlot('bar', bar);
 
-function optionChanged(selectedID){
-    // console.log(selectedID);
-
-    TopSamples(selectedID);
-    DemographicInfo(selectedID);
-    BubbleChart(selectedID);
-    // LoadCharts(selectedID);
-}
-
-function TopSamples(id)
-{
-  d3.json("samples.json").then(data => {
-      // console.log(data);
-      let filteredData =data.samples.filter(obj => obj.id == id);
-
-      // console.log(filteredData);
-
-      let otu_ids = filteredData[0].otu_ids.map(otu=> "OTU " + otu);
-      let otu_labels = filteredData[0].otu_labels;
-      let sample_values = filteredData[0].sample_values;
-      // console.log(otu_ids);
-      // console.log(otu_labels);
-      // console.log(sample_values);
-
-      let otu_ids_slice = otu_ids.slice(0,10).reverse();
-      let otu_labels_slice = otu_labels.slice(0,10).reverse();
-      let sample_values_slice = sample_values.slice(0,10).reverse();
-
-      let traceBar = 
-      {
-        x: sample_values_slice,
-        y: otu_ids_slice,
-        text: otu_labels_slice,
-        type: "bar",
-        orientation: "h"
-      };
-
-      // Data trace array
-      let traceData = [traceBar];
-
-      // Apply the group barmode to the layout
-      let layout = 
-      {
-        title: "Top 10 Samples"
-      };
-
-      // Render the plot to the div tag with id "plot"
-      Plotly.newPlot("bar", traceData, layout);
-    }
-  );
-}
 
     //Now do the bubble chart, but it uses all of the data, also don't need to do the .map
-
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text:otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values
+      }
+    };
+    
+    let bubble = [trace1];
+    
+    let layout = {
+      title: 'Belly Button Biodiversity',
+      showlegend: false,
+      height: 600,
+      width: 1000
+    };
+    
+    Plotly.newPlot('bubble', bubble, layout);
     
     //Now do the metadata demo panel at sample-metadata
     let filteredMetaData = data.metadata.filter(obj => obj.id == id);
@@ -109,6 +77,9 @@ function TopSamples(id)
     console.log(filteredMetaData);
 
     let panel = d3.select("#sample-metadata");
+
+    for (const [key, value] of Object.entries(filteredMetaData[0])) {
+      panel.append("h5").text(key + ": " + value)
 
     // Look up looping through an Object.entries with a forEach [key, value]
     // panel.append("h5").text(key + ": " + value)
@@ -130,6 +101,7 @@ d3.json("samples.json").then((data) => {
 })
 
 
-function optionChanged(selectedID) 
-
-  loadCharts(selectedID)}
+function optionChanged(selectedID){
+  console.log(selectedID)
+  loadCharts(selectedID)
+}
